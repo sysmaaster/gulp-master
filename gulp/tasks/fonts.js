c442f137cd5bf37e3e2492e5f44aca0ca3780e24
sasa <sysmaaster@gmail.com>
2022-07-19 19:14:22 +0300

    fix строчка fs.appendFile

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -78,14 +78,7 @@ export const fontsStyle = () => {
                    } else {
                       fontWeight = 400;
                    }
-                   fs.appendFile(fontsFile,
-                      `@font-face {
-                   font-family: ${fontName};
-                   font-display: swap;
-                   src: url("../fonts/${fontFileName}.woff2") format("woff2"), url("../fonts/${fontFileName}.woff") format("woff");
-                   font-weight: ${fontWeight};
-                   font-style: normal;
-                }\r\n`, cb);
+                   fs.appendFile(fontsFile, `@font-face{\n\tfont-family: ${fontName};\n\tfont-display: swap;\n\tsrc: url("../fonts/${fontFileName}.woff2") format("woff2"), url("../fonts/${fontFileName}.woff") format("woff");\n\tfont-weight: ${fontWeight};\n\tfont-style: normal;\n}\r\n`, cb);
                    //
                    newFileOnly = fontFileName;
                 }
