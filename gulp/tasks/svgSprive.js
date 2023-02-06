b7e6c1ef0ce9d55a31c04560e6ddbada853f32ac
Oleksandr A <36313826+sysmaaster@users.noreply.github.com>
2023-02-06 23:41:14 +0200

    Update svgSprive.js
    FIX BUG  path"../sprite.svg",

--- gulp/tasks/svgSprive.js
+++ gulp/tasks/svgSprive.js
@@ -10,7 +10,7 @@ export const svgSprive = () => {
     .pipe(svgSprite({
         mode: {
             stack: {
-                sprite: `../icons/icons.svg`,
+                sprite: "../sprite.svg",
                 example : true
             }
         },
